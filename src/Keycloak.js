dfdc9b783d9250e321a3e59d248764cb974da17a
Tataridis Paris Rafail <paris.tataridis@gmail.com>
2023-07-20 12:22:39 +0300

    url change

--- src/Keycloak.js
+++ src/Keycloak.js
@@ -2,7 +2,7 @@ import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak(
     {
-        url: 'http://localhost:8080/' ,
+        url: 'http://172.30.1.224:8080/' ,
         realm: 'myrealm' ,
         clientId: 'myclient' ,
     }
