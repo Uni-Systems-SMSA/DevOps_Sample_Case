aaec4c2f941f6663e73c08cf1495b480c9ee9e26
iliasdodoros <iliasdodoros@gmail.com>
2023-08-23 17:04:01 +0300

    update deployscript

--- scripts/deploy-k8-script.sh
+++ scripts/deploy-k8-script.sh
@@ -1,4 +1,4 @@
-minikube mount /root/DevOps_Sample_Case/keycloak:/root/DevOps_Sample_Case/keycloak &
+minikube mount /root/DevOps_Sample_Case/keycloak:/home/docker &
 
 kubectl set image deployments/frontend-deployment frontend-deployment=172.30.1.224:5050/devops-sample:latest 
 
