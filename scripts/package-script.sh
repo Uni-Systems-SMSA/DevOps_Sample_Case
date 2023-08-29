6c923b1240f7d71b4adbd70e633a9ae8c1e87fab
iliasdodoros <71443918+iliasdodoros@users.noreply.github.com>
2023-08-29 15:19:55 +0300

    Update package-script.sh

--- scripts/package-script.sh
+++ scripts/package-script.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
+cd /root/
 git clone https://github.com/Uni-Systems-SMSA/DevOps_Sample_Case.git
 
-cd DevOps_Sample_Case
-git checkout Front-end-features
-
+cd /root/DevOps_Sample_Case
+git checkout main
 git pull
 
 docker build -t 172.30.1.224:5050/devops-sample:$1 -t 172.30.1.224:5050/devops-sample:latest .
 
 docker push 172.30.1.224:5050/devops-sample --all-tags
-
