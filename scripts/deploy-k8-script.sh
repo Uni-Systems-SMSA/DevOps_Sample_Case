b9fa725dd9b30ec499be19d1fa831cb97214d4c8
iliasdodoros <71443918+iliasdodoros@users.noreply.github.com>
2023-09-01 12:27:58 +0300

    Update deploy-k8-script.sh

--- scripts/deploy-k8-script.sh
+++ scripts/deploy-k8-script.sh
@@ -1,6 +1,4 @@
-minikube mount /root/DevOps_Sample_Case/keycloak:/home/docker &
-
-kubectl set image deployments/frontend-deployment frontend-deployment=172.30.1.224:5050/devops-sample:latest 
+kubectl set image deployments/frontend-deployment frontend-deployment=172.30.1.224:5050/devops-sample:$1
 
 kubectl port-forward --address=0.0.0.0 services/frontend-load 3001 &
 kubectl port-forward --address=0.0.0.0 services/keycloak-service 8080 &
